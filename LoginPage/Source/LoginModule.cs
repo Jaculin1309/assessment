ea6bd9e2ad01307d0cba15b644b4f65f0ba9d78c
Jaculin1309 <140996537+Jaculin1309@users.noreply.github.com>
2023-07-31 10:57:40 +0530

    Add files via upload

---
+++ LoginPage/Source/LoginModule.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage.Source
+{
+    public class LoginModule
+    {
+        public IWebDriver Driver { get; private set; }
+
+        // Constructor
+        public LoginModule(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        // Methods to interact with the login page
+        public void EnterUsername(string username)
+        {
+            Driver.FindElement(By.Id("user-name")).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            Driver.FindElement(By.Id("password")).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            Driver.FindElement(By.Id("login-button")).Click();
+        }
+
+        // Add any other helper methods or assertions related to the login page here
+    }
+}
---
+++ LoginPage/Tests/LoginTest.cs
@@ -0,0 +1,48 @@
+﻿using LoginPage.Source;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace LoginPage.Tests
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private LoginModule loginModule;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            loginModule = new LoginModule(new ChromeDriver());
+        }
+
+        [Test]
+        [TestCase("standard_user", "secret_sauce")]
+        [TestCase("locked_out_user", "secret_sauce")]
+        [TestCase("problem_user", "secret_sauce")]
+        [TestCase("performance_glitch_user", "secret_sauce")]
+        public void Test_Login_With_Different_Users(string username, string password)
+        {
+            // Arrange: Navigate to the login page
+            Console.WriteLine("Navigating to the login page.");
+            loginModule.Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Act: Perform the login using LoginModule and different users
+            Console.WriteLine($"Performing login with username: {username} and password: {password}");
+            loginModule.EnterUsername(username);
+            loginModule.EnterPassword(password);
+            loginModule.ClickLoginButton();
+
+            // Assert: Check if the login is successful or failed based on the user
+            if (username == "locked_out_user")
+            {
+                NUnit.Framework.Assert.IsTrue(loginModule.Driver.Url.Contains("locked_out"), "Login was expected to fail for locked_out_user.");
+            }
+            else
+            {
+                NUnit.Framework.Assert.IsTrue(loginModule.Driver.Url.Contains("dashboard"), "Login was not successful. Redirect to dashboard failed.");
+            }
+        }
+    }
+}
